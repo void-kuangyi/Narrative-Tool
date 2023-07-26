47d03899943ae11be9b555ccee4c88c896f7c853
kuangyi <xingkuangyi@gmail.com>
2023-07-26 23:44:22 +0200

    add the border around the tree

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ function App() {
   // var data = window.narrativeSurveyDataJson;
   var data = require("./data.json");
   const transformedData = transformData(data);
-  const foreignObjectProps = { width: 200, height: 300, x: 50, y: -30 };
+  const foreignObjectProps = { width: 400, height: 300, x: 50, y: -10 };
   return (
     <div className="App">
-      <Tree
-        data={transformedData}
-        pathClassFunc={() => "custom-link"}
-        orientation="vertical"
-        hasInteractiveNodes="true"
-        renderCustomNodeElement={(rd3tProps) =>
-          renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
-        }
-        nodeSize={{ x: 300, y: 300 }}
-        translate={{ x: window.screen.width / 2, y: 100 }}
-      />
+      <span
+        style={{
+          fontSize: "30px",
+          color: "white",
+          fontWeight: "600",
+        }}
+      >
+        TwineJS Researcher Web
+      </span>
+      <div className="Tree">
+        <Tree
+          data={transformedData}
+          pathClassFunc={() => "custom-link"}
+          orientation="vertical"
+          hasInteractiveNodes="true"
+          renderCustomNodeElement={(rd3tProps) =>
+            renderForeignObjectNode({ ...rd3tProps, foreignObjectProps })
+          }
+          nodeSize={{ x: 300, y: 300 }}
+          translate={{ x: window.screen.width / 2, y: 100 }}
+        />
+      </div>
     </div>
   );
 }
